effbf5fb5e7d310e8c42ec9da55ed38e5bfbf8ee
Ciarán Ó hAoláin <ciaran@cohaolain.ie>
2020-03-20 18:32:19 +0000

    Define deployment logic

--- migrations/1_initial_deployment.js
+++ migrations/1_initial_deployment.js
@@ -1,10 +1,58 @@
 const PEGStableCoin = artifacts.require("PEG");
 const FakeMedianiser = artifacts.require("Medianiser");
 
-module.exports = function (deployer) {
-
-  deployer.deploy(FakeMedianiser, "0x00000000000000000000000000000000000000000000000ca6cc517ff0aa0000").then(
-    DeployedFakeMedianiser => deployer.deploy(PEGStableCoin, DeployedFakeMedianiser.address, 60 * 60, { value: 10 ** 18 })
-  )
-
+module.exports = function(deployer, network, accounts) {
+  if (network == "development") {
+    deployer
+      .deploy(
+        FakeMedianiser,
+        "0x" +
+          web3.utils
+            .toHex(web3.utils.toWei("200")) // Assume 1 Ether = $200
+            .substr(2)
+            .padStart(64, "0")
+      )
+      .then(DeployedFakeMedianiser =>
+        deployer.deploy(
+          PEGStableCoin,
+          DeployedFakeMedianiser.address,
+          60 * 60,
+          {
+            value: web3.utils.toWei("1")
+          }
+        )
+      );
+  } else if (network == "ropsten") {
+    deployer
+      .deploy(
+        FakeMedianiser,
+        "0x" +
+          web3.utils
+            .toHex(web3.utils.toWei("200")) // Assume 1 Ether = $200
+            .substr(2)
+            .padStart(64, "0"),
+        { from: accounts[0] }
+      )
+      .then(DeployedFakeMedianiser =>
+        deployer.deploy(
+          PEGStableCoin,
+          DeployedFakeMedianiser.address,
+          60 * 60,
+          {
+            value: web3.utils.toWei("1"),
+            from: accounts[1]
+          }
+        )
+      );
+  } else if (network == "mainnet") {
+    deployer.deploy(
+      PEGStableCoin,
+      "0x729D19f657BD0614b4985Cf1D82531c67569197B",
+      60 * 60,
+      {
+        value: web3.utils.toWei("1"),
+        from: accounts[1]
+      }
+    );
+  }
 };
